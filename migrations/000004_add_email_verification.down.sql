be909d66585cbfb5ad6c291c20c70ab108041b44
Javier Fernández <franciscojavier.fernandezcabanas@gi-de.com>
2025-06-22 14:23:22 +0200

    update migrations

--- migrations/000004_add_email_verification.down.sql
+++ migrations/000004_add_email_verification.down.sql
@@ -2,16 +2,21 @@
 -- Date: 2024-06-20
 -- Description: Removes email verification fields and verification_tokens table
 
--- 1. Drop indexes
+-- 1. Drop trigger if table exists (using DO block for conditional execution)
+DO $$ 
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'verification_tokens') THEN
+        DROP TRIGGER IF EXISTS update_verification_tokens_updated_at ON verification_tokens;
+    END IF;
+END $$;
+
+-- 2. Drop indexes
 DROP INDEX IF EXISTS idx_verification_tokens_type_user;
 DROP INDEX IF EXISTS idx_users_email_verified;
 DROP INDEX IF EXISTS idx_verification_tokens_deleted_at;
 DROP INDEX IF EXISTS idx_verification_tokens_expires_at;
 DROP INDEX IF EXISTS idx_verification_tokens_token;
 DROP INDEX IF EXISTS idx_verification_tokens_user_id;
-
--- 2. Drop trigger
-DROP TRIGGER IF EXISTS update_verification_tokens_updated_at ON verification_tokens;
 
 -- 3. Drop verification_tokens table
 DROP TABLE IF EXISTS verification_tokens;
